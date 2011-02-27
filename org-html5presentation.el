feec8fdc24a6fa981652edd9774e7d1ca32742df
Takuto Wada <takuto.wada@gmail.com>
2011-02-28 00:25:29 +0900

    extract copyright section to defcustom

--- org-html5presentation.el
+++ org-html5presentation.el
@@ -45,6 +45,20 @@
   :tag "Org Export HTML5"
   :group 'org-export)
 
+(defcustom org-export-html5presentation-copyright-section "<!--
+  Copyright %s %s.
+ 
+  Original slides: Marcin Wichary (mwichary@google.com)
+  Modifications: Chrome DevRel Team (chrome-devrel@googlegroups.com)
+                 Alex Russell (slightlyoff@chromium.org)
+                 Brad Neuberg
+                 Takuto Wada (takuto.wada@gmail.com)
+-->"
+  "Format for the copyright section.
+Should contain a two instances of %s. The first will be replaced with current year, the second one will be replaced with author name"
+  :group 'org-export-html5presentation
+  :type 'string)
+
 (defcustom org-export-html5presentation-footnotes-section "<div id=\"footnotes\">
 <h2 class=\"footnotes\">%s: </h2>
 <div id=\"text-footnotes\">
@@ -897,27 +911,7 @@ PUB-DIR is set, use this as the publishing directory."
         ;; File header
         (insert (format "
 <!DOCTYPE html> 
-<!--
-  Copyright 2011 %s.
- 
-  Licensed under the Apache License, Version 2.0 (the \"License\");
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
- 
-     http://www.apache.org/licenses/LICENSE-2.0
- 
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an \"AS IS\" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
- 
-  Original slides: Marcin Wichary (mwichary@google.com)
-  Modifications: Chrome DevRel Team (chrome-devrel@googlegroups.com)
-                 Alex Russell (slightlyoff@chromium.org)
-                 Brad Neuberg
-                 Takuto Wada (takuto.wada@gmail.com)
---> 
+%s
 <html>  
   <head> 
     <meta charset=\"utf-8\" /> 
@@ -939,7 +933,13 @@ PUB-DIR is set, use this as the publishing directory."
   <body>
     <div class=\"presentation\">
       <div id=\"presentation-counter\">Loading...</div> 
-      <div class=\"slides\">" author title (file-name-nondirectory filename)))
+      <div class=\"slides\">"
+                        (format org-export-html5presentation-copyright-section
+                                (format-time-string "%Y" (current-time))
+                                author)
+                        title
+                        (file-name-nondirectory filename)
+                        ))
 ;;         (insert (format
 ;;                  "%s
 ;; <!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Strict//EN\"
